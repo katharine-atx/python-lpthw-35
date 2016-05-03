a9ad19bfec26f9d79941f2da406b537543801c26
katharine-atx <rovinsky.katharine@gmail.com>
2016-05-03 12:49:13 -0500

    Ex 41 with notes on Python3 changes

--- ex41_v35.py
+++ ex41_v35.py
@@ -1,26 +1,40 @@
-# Version 2.7 to 3.5: print(), input() instead of raw_input(), 
-#{}/.format() instead of % formatter
+# This exercise is meant to help you quiz yourself on object-oriented phrases in python.
+# Run note: default run outputs object-oriented phrases from PHRASES dict first, followed by English.
+# Run as ex41_v35.py english to get the inverse order (English-language translations first).
+
+# Version 2.7 to 3.5: Several types of changes required...
+# Basic syntax: print(), input() instead of raw_input(), 
+# 	{}/.format() instead of % formatter.
+# Library note: use urllib.request as library for urlopen
+# Changes to default classes: dict key and urlopen()
+#	In version 3, dict.keys() output is set-like and you can't call objects by index as desired
+# 	in the Try/While True section of this exercise. Instead use a list() for the key. More in:
+# http://stackoverflow.com/questions/18552001/accessing-dict-keys-element-by-index-in-python3
+# 	Loading the WORDS requires decoding from ASCII so you get string, not bytes. More in:
+# http://stackoverflow.com/questions/16699362/python3-error-typeerror-cant-convert-bytes-object-to-str-implicitly
+
 
 import random
-from urllib import urlopen
+from urllib.request import urlopen
 import sys
 
 WORD_URL = "http://learncodethehardway.org/words.txt"
 WORDS = []
 
+# PHRASES dict...
 PHRASES = {
 	"class %%%(%%%):":
 		"Make a class named %%% that is-a %%%.",
-	"class %%%(object):\n\tdef __init(self, ***)":
+	"class %%%(object):\n\tdef __init__(self, ***)":
 		"class %%% has-a __init__ that takes self and *** parameters.",
 	"class %%%(object):\n\tdef ***(self, @@@)":
 		"class %%% has-a function named *** that takes self and @@@ parameters.",
 	"*** = %%%()":
 		"Set *** to an instance of class %%%.",
 	"***.***(@@@)":
-			"From *** get the *** funstion, and call it with parameters self, @@@.",
-		"***.*** = '***'":
-			"From *** get the *** attribute and set it to '***'"
+		"From *** get the *** function, and call it with parameters self, @@@.",
+	"***.*** = '***'":
+		"From *** get the *** attribute and set it to '***'"
 }
 
 # Drilling phrases first...
@@ -30,8 +44,8 @@ else:
 	PHRASE_FIRST = False
 	
 # Loading the words from the URL...
-for word in urlopen(WORD_URL).readlines():
-	WORDS.append(word.strip())
+for word in urlopen(WORD_URL):
+	WORDS.append(word.strip().decode('ascii'))
 
 def convert(snippet, phrase):
 	class_names = [w.capitalize() for w in
@@ -41,7 +55,7 @@ def convert(snippet, phrase):
 	param_names = []
 	
 	for i in range(0, snippet.count("@@@")):
-		param_count = rndom.randint(1,3)
+		param_count = random.randint(1,3)
 		param_names.append(', '.join(random.sample(WORDS, param_count)))
 		
 	for sentence in snippet, phrase:
@@ -63,12 +77,16 @@ def convert(snippet, phrase):
 		
 	return results
 	
-# continue until user hits CTRL-D...
+# Adding break option with input() == "exit"...
+print("\n******Type exit for break, ENTER to continue******\n")
 try:
 	while True:
-		snippets = PHRASES.keys()
+		snippets = list(PHRASES)
 		random.shuffle(snippets)
 		
+		if input() == "exit":
+			break
+			
 		for snippet in snippets:
 			phrase = PHRASES[snippet]
 			question, answer = convert(snippet, phrase)
@@ -79,6 +97,6 @@ try:
 			
 			input("> ")
 			print("ANSWER: {}\n\n" .format(answer))
-	except EOFError:
-		print("\nBye")
 		
+except EOFError:
+	print("\nBye")		
