b8881d071cb062bca28e04b31e2ced61a3da1c96
katharine-atx <rovinsky.katharine@gmail.com>
2016-05-18 17:52:18 -0500

    Ex 43 game framework - corrections on indents

--- ex43_v35.py
+++ ex43_v35.py
@@ -1,5 +1,5 @@
 # Version 2.7 to 3.5: print(), input() v. raw_input(), use {}/.format()
-# rather than % formatters.
+# rather than % formatters. 
 
 from sys import exit
 from random import randint
@@ -45,17 +45,17 @@ class CentralCorridor(Scene):
 		
 		action = input("What do you do?\n\n")
 	
-		if action == "shoot!"
+		if action == "shoot!":
 			# Add scene narrative via print()...
 			print("Story point, resulting in player death...")
 			return 'death'
 			
-		elif action == "dodge!"
+		elif action == "dodge!":
 			# Add scene narrative via print()...
 			print("Story point, sadly also ending in death.")
 			return 'death'
 			
-		elif action == "tell a joke"
+		elif action == "tell a joke":
 			# Add scene narrative via print()...
 			print("Story point, this works, actually.")
 			return 'laser_weapon_armory'
@@ -72,84 +72,84 @@ class LaserWeaponArmory(Scene):
 		code = "{}{}{}" .format(randint(1,9))
 		guess = input("[keypad]> ")
 		
-	if guess == code:
-		# Add scene narrative via print()...
-		print("How often would someone actually guess the",
-		"correct code? Is so, go on to the bridge.")
-		return 'the_bridge'
-	else:
-		# Add scene narrative via print()...
-		print("So you die in this case. Not a lot of winners here.")
-		return 'death'
+		if guess == code:
+			# Add scene narrative via print()...
+			print("How often would someone actually guess the",
+			"correct code? If so, go on to the bridge.")
+			return 'the_bridge'
+		else:
+			# Add scene narrative via print()...
+			print("So you die in this case. Not a lot of winners here.")
+			return 'death'
 			
 class TheBridge(Scene):
 	
 	def enter(self):
-	# Add scene narrative via print()...
-	print("So, you're not the only one on the bridge. Do you",
-	"....throw the bomb OR slowly place the bomb?")
-	
-	action = input("> ")
-	
-	if action == "throw the bomb":
 		# Add scene narrative via print()...
-		print("It backfires. You die in this one.")
-		return 'death'
-		
-	elif action == "slowly place the bomb":
-		# Add scene narrative via print()...
-		print("Not too surprising this is the less deadly choice",
-		"Now to the escape pod!")
-		return 'escape pod'
-	else:
-		print("DOES NOT COMPUTE!")
-		return the_bridge
-		
-	class EscapePod(Scene):
+		print("So, you're not the only one on the bridge.",
+		"Do you throw the bomb OR slowly place the bomb?")
 	
-		def enter(self):
+		action = input("> ")
+	
+		if action == "throw the bomb":
 			# Add scene narrative via print()...
-			print("Time to guess again.... Which pod # do you take?",
-			"There are 5 here.")
-			good_pod = randint(1,5)
-			guess = input("[pod #]> ")
+			print("It backfires. You die in this one.")
+			return 'death'
 			
-			if int(guess) != good_pod:
-				#Add scene narrative via print()...
-				print("You jump into pod {} and hit EJECT." .format(guess))
-				print("You die in this one however...")
-				return 'death'
-				
-			else:
-				#Add scene narrative via print()...
-				print("You jump into pod {} and hit EJECT." .format(guess))
-				return 'finished'
-				
-		class Finished(Scene):
-			def enter(self):
-				print("Amazing! You win!!!")
-				return 'finished'
-				
-		class Map(object):
+		elif action == "slowly place the bomb":
+			# Add scene narrative via print()...
+			print("Not too surprising this is the less deadly choice",
+			"Now to the escape pod!")
+			return 'escape pod'
+		else:
+			print("DOES NOT COMPUTE!")
+			return the_bridge
 		
-			scenes = {
-				'central_corridor': CentralCorridor(),
-				'laser_weapon_armory': LaserWeaponArmory(),
-				'the_bridge': TheBridge(),
-				'escape_pod': EscapePod(),
-				'death': Death(),
-				'finished': Finished(),
-			}
+class EscapePod(Scene):
+	
+	def enter(self):
+		# Add scene narrative via print()...
+		print("Time to guess again.... Which pod # do you take?",
+		"There are 5 here.")
+		good_pod = randint(1,5)
+		guess = input("[pod #]> ")
 			
-			def __init__(self, start_scene):
-				self.start_scene = start_scene
+		if int(guess) != good_pod:
+			#Add scene narrative via print()...
+			print("You jump into pod {} and hit EJECT." .format(guess))
+			print("You die in this one however...")
+			return 'death'
 				
-			def next_scene(self, scene_name):
-				val = Map.scenes.get(scene_name)
-				return val
+		else:
+			#Add scene narrative via print()...
+			print("You jump into pod {} and hit EJECT." .format(guess))
+			return 'finished'
 				
-			def opening_scene(self):
-				return self.nect_scene(self.start_scene)
+class Finished(Scene):
+	def enter(self):
+		print("Amazing! You win!!!")
+		return 'finished'
+				
+class Map(object):
+		
+	scenes = {
+		'central_corridor': CentralCorridor(),
+		'laser_weapon_armory': LaserWeaponArmory(),
+		'the_bridge': TheBridge(),
+		'escape_pod': EscapePod(),
+		'death': Death(),
+		'finished': Finished(),
+	}
+			
+	def __init__(self, start_scene):
+		self.start_scene = start_scene
+				
+	def next_scene(self, scene_name):
+		val = Map.scenes.get(scene_name)
+		return val
+				
+	def opening_scene(self):
+		return self.next_scene(self.start_scene)
 				
 a_map = Map('central_corridor')
 a_game = Engine(a_map)
